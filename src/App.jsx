bcce0144475ab518c4297013f63d99231be489bf
Engrloverboy <samsonanu@yahoo.com>
2023-07-27 10:40:40 +0100

    nav

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Glovo from "./assets/image/Glovo_logo.png"
+function App() {
+  return (
+    <nav>
+      <section className="container">
+        
+          <img src={Glovo} alt="" />
+
+          <button>Get Started</button>
+      </section>
+    </nav>
+  );
+}
+
+export default App;
